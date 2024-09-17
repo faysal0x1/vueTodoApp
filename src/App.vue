6aa11140c685a6c4970fb5f1608a8035c60f8b02
MD. Faysal Rahman <faysal35-3119@diu.edu.bd>
2024-09-18 04:50:52 +0600

    Full initial

--- src/App.vue
+++ src/App.vue
@@ -25,12 +25,23 @@ onMounted(() => {
             <div class="row mb-3">
               <form action="">
                 <div class="col-md-6">
-                  <label for="" class="form-label"> Add New Task</label>
-                  <input type="text" class="form-control" name="" id="">
+                  <label for="" class="form-label">
+                    <span v-if="!todoStore.isEdit"> Add New Task</span>
+                    <span v-else="todoStore.isEdit"> Update Task</span>
+
+                  </label>
+                  <input type="text" class="form-control" name="" id="" v-model="todoStore.todoForm.title">
                 </div>
-                <div class="mt-3">
-                  <input type="button" value="Add Task" class="btn btn-success">
+
+                <div class="mt-3" v-if="!todoStore.isEdit">
+                  <input type="button" value="Add Task" class="btn btn-success" @click.prevent="todoStore.addTodo()">
+                </div> 
+                  <div class="mt-3" v-else="todoStore.isEdit">
+                  <input type="button" value="Update Task" class="btn" 
+                  :class="todoStore.todoForm.completed ? 'btn-success' : 'btn-warning'"
+                  @click.prevent="todoStore.updateTodo(todoStore.editId)">
                 </div>
+
               </form>
             </div>
 
@@ -44,13 +55,27 @@ onMounted(() => {
             <ul class="list-group">
               <li class="list-group-item d-flex justify-content-between align-items-center"
                 v-for="todo in todoStore.todos" :key="todo.id">
-                <span :class="todo.completed ? 'text-decoration-line-through':'' ">
-                  {{ todo.title }}
-                </span>
-              
-                <a href="" class="btn">
-                  <i class="fa fa-trash"></i>
-                </a>
+
+                <div class="mx-2">
+                  <input type="checkbox" name="" id="" class="form-check-input me-3">
+                  <span :class="todo.completed ? 'text-decoration-line-through' : ''">
+                    {{ todo.title }}
+                  </span>
+                </div>
+
+                <div class="ms-auto">
+
+                  <a href="" class="btn" @click.prevent="todoStore.getTodo(todo.id)">
+                    <i class="fa fa-edit
+                    "></i>
+                  </a>
+
+                  <button class="btn" @click.prevent="todoStore.deleteTodo(todo.id)">
+                    <i class="fa fa-trash"></i>
+                  </button>
+                </div>
+
+
 
               </li>
 
--- src/stores/todo.js
+++ src/stores/todo.js
@@ -4,14 +4,69 @@ import axios from "axios";
 export const useTodoStore = defineStore("todo", {
   state: () => ({
     todos: [],
+    todoForm: {
+      title: "",
+      userId: 1,
+      completed: false,
+    },
+    editId: 0,
+    isEdit: false,
   }),
   actions: {
     async getAllTodos() {
-      const {data} = await axios.get("https://jsonplaceholder.typicode.com/todos")
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos"
+      );
+
+      this.todos = data;
+    },
+
+    async addTodo() {
+      const { data } = await axios.post(
+        "https://jsonplaceholder.typicode.com/todos",
+        this.todoForm
+      );
+      this.todos.push(data);
+      this.todos.reverse();
+
+      this.todoForm.title = "";
+    },
+
+    async deleteTodo(id) {
+      await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+
+    async getTodo(id) {
+      const { data } = await axios.get(
+        `https://jsonplaceholder.typicode.com/todos/${id}`
+      );
+
+      this.todoForm.title = data.title;
+      this.todoForm.userId = data.userId;
+      this.todoForm.completed = data.completed;
+      this.editId = data.id;
+      this.isEdit = true;
+    },
+
+    async updateTodo(id) {
+
+      console.log(id);
       
-        this.todos =data
-        
-    
+
+      const { data } = await axios.put(
+        `https://jsonplaceholder.typicode.com/todos/${id}`,
+        this.todoForm
+      );
+
+      this.todos.push(data);
+      this.todos.reverse();
+      this.isEdit = false;
+
+      this.todoForm.title = "";
+      this.todoForm.userId = 1;
+      this.todoForm.completed = false;
     },
   },
   getters: {},
